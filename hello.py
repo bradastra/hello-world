7954350a5615dbdac00078eeb91bc0d1286b5d9e
bradastra <bradastra.ai@gmail.com>
2023-09-27 13:17:04 -0600

    Added interactions for favorite color and hobby.

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
 name = input("What's your name? ")
 print(f"Hello, {name}! Welcome to the world of Python.")
 
+color = input("What's your favorite color? ")
+print(f"Ah, {color}! That's a great choice, {name}.")
+
+hobby = input("What's your favorite hobby? ")
+print(f"Wow! {hobby} sounds like a lot of fun.")
+

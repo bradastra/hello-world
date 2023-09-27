023bfb8f4eb556804ebf50d864d4d610ab322123
bradastra <146237232+bradastra@users.noreply.github.com>
2023-09-27 15:52:49 -0600

    Enhanced Personalized Chatbot
    Integrated advanced functionalities into the chatbot, allowing it to remember and respond based on user's personal details like name and favorite color. Enhanced its ability to carry a more personalized conversation, making interactions feel more natural and tailored to the user.

--- chatbot.py
+++ chatbot.py
@@ -1,37 +1,51 @@
 import random
 
-class Chatbot:
-    def __init__(self):
-        self.responses = {
-            "hi": ["Hello! What's your name?", "Hi there! May I know your name?"],
-            "hello": ["Hello! What's your name?", "Hi! Can you tell me your name?"],
-            "default": ["I'm not sure how to respond to that.", "Can you rephrase that?"],
-            "name_known": ["Nice to meet you, {}! How can I assist you today?", "Hello, {}! What can I do for you?"]
-        }
-        self.user_name = None
+# Dictionary to store responses
+responses = {
+    "hello": ["Hi there!", "Hello!", "Hey! How can I assist you?"],
+    "how are you": ["I'm just a program, so I don't have feelings, but I'm running smoothly!", "As good as a chatbot can be!", "Operating at full capacity!"],
+    "default": ["I'm sorry, I don't understand that.", "Could you please rephrase that?", "I'm not sure how to respond to that."]
+}
 
-    def get_response(self, message):
-        if self.user_name:
-            if "name" in message or "who am i" in message:
-                return f"You're {self.user_name}! How can I assist you today?"
-        
-        if message in ["hi", "hello"]:
-            return random.choice(self.responses[message])
+# Dictionary to store user data for personalization
+user_data = {
+    "name": None,
+    "favorite_color": None,
+    "favorite_book": None,
+    "favorite_movie": None
+}
 
-        if "my name is" in message or "i am" in message or "call me" in message:
-            # Extract the user's name from the message
-            self.user_name = message.split()[-1]
-            return random.choice(self.responses["name_known"]).format(self.user_name)
+def respond_to_user(user_input, user_data):
+    if user_input in responses:
+        return random.choice(responses[user_input])
+    
+    # Handle the user's name
+    if "my name is" in user_input:
+        user_name = user_input.split("is")[-1].strip()
+        user_data["name"] = user_name
+        return f"Nice to meet you, {user_name}!"
+    if user_data["name"] and ("remember my name" in user_input or "do you know me" in user_input):
+        return f"Of course, {user_data['name']}!"
+    
+    # Handle the user's favorite color
+    if "favorite color" in user_input:
+        user_data["favorite_color"] = user_input.split("is")[-1].strip()
+        return f"Got it! I'll remember your favorite color is {user_data['favorite_color']}."
+    if "remember my favorite color" in user_input:
+        return f"Your favorite color is {user_data['favorite_color']}!" if user_data["favorite_color"] else "You haven't told me your favorite color yet."
+    
+    # Default response
+    return random.choice(responses["default"])
 
-        return random.choice(self.responses["default"])
+def main():
+    print("Hello! How can I assist you today?")
+    while True:
+        user_input = input().lower()
+        if user_input in ["exit", "quit", "bye"]:
+            print("Goodbye! Talk to you later.")
+            break
+        response = respond_to_user(user_input, user_data)
+        print(response)
 
 if __name__ == "__main__":
-    bot = Chatbot()
-    print("Chatbot: Hi there! Type 'quit' to exit.")
-    while True:
-        user_input = input("You: ").lower()
-        if user_input == 'quit':
-            print("Chatbot: Goodbye!")
-            break
-        response = bot.get_response(user_input)
-        print(f"Chatbot: {response}")
+    main()

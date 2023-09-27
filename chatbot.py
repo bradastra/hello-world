4a6971a4f1d91f9c9a29f80cd068ca34568ade2e
bradastra <146237232+bradastra@users.noreply.github.com>
2023-09-27 15:43:45 -0600

    Enhanced User Name Recognition
    This update introduces improved logic to the chatbot, allowing it to remember and recognize the user's name during an active session. Users can now ask the chatbot to recall their name, enhancing the interaction and personalization of the chatbot. Additionally, the code has been optimized for better response handling and more intuitive conversations.

--- chatbot.py
+++ chatbot.py
@@ -1,55 +1,37 @@
 import random
 
-def chatbot_response(text, user_data):
-    text = text.lower()
-    
-    # Bot greeting responses
-    greetings = ["hello", "hi", "hey", "sup", "yo"]
-    greeting_responses = ["Hello!", "Hi there!", "Hey!", "Hi! How can I help you?"]
+class Chatbot:
+    def __init__(self):
+        self.responses = {
+            "hi": ["Hello! What's your name?", "Hi there! May I know your name?"],
+            "hello": ["Hello! What's your name?", "Hi! Can you tell me your name?"],
+            "default": ["I'm not sure how to respond to that.", "Can you rephrase that?"],
+            "name_known": ["Nice to meet you, {}! How can I assist you today?", "Hello, {}! What can I do for you?"]
+        }
+        self.user_name = None
 
-    # Farewells
-    farewells = ["bye", "goodbye", "see you", "later"]
-    farewell_responses = ["Goodbye!", "See you later!", "Take care!"]
+    def get_response(self, message):
+        if self.user_name:
+            if "name" in message or "who am i" in message:
+                return f"You're {self.user_name}! How can I assist you today?"
+        
+        if message in ["hi", "hello"]:
+            return random.choice(self.responses[message])
 
-    # Questions about the bot
-    bot_questions = ["how are you", "are you real", "who are you"]
-    bot_responses = [
-        "I'm just a program, so I don't have feelings, but I'm functioning properly!",
-        "I'm virtual, not a real person.",
-        "I'm a simple chatbot, here to assist you!"
-    ]
+        if "my name is" in message or "i am" in message or "call me" in message:
+            # Extract the user's name from the message
+            self.user_name = message.split()[-1]
+            return random.choice(self.responses["name_known"]).format(self.user_name)
 
-    # Check and respond
-    if text in greetings:
-        return random.choice(greeting_responses)
-    elif text in farewells:
-        return random.choice(farewell_responses)
-    elif any(bot_q in text for bot_q in bot_questions):
-        return random.choice(bot_responses)
-    elif "name" in text and "your" in text:
-        return "I'm just a chatbot, but you can call me ChatGPT!"
-    elif "my name is" in text:
-        user_data["name"] = text.split("is")[-1].strip()
-        return f"Nice to meet you, {user_data['name']}!"
-    elif "remember my name" in text:
-        if "name" in user_data:
-            return f"Of course, your name is {user_data['name']}!"
-        else:
-            return "I'm sorry, I don't recall your name. What is it?"
-    else:
-        return "I'm not sure how to respond to that. Can you rephrase?"
-
-def main():
-    user_data = {}  # Store user-specific data like name
-    print("Chatbot: Hello! If you want to exit, just say 'bye'.")
-    
-    while True:
-        user_input = input("You: ").strip()
-        if user_input.lower() in ["bye", "goodbye", "exit"]:
-            print("Chatbot: Goodbye! Have a great day!")
-            break
-        response = chatbot_response(user_input, user_data)
-        print(f"Chatbot: {response}")
+        return random.choice(self.responses["default"])
 
 if __name__ == "__main__":
-    main()
+    bot = Chatbot()
+    print("Chatbot: Hi there! Type 'quit' to exit.")
+    while True:
+        user_input = input("You: ").lower()
+        if user_input == 'quit':
+            print("Chatbot: Goodbye!")
+            break
+        response = bot.get_response(user_input)
+        print(f"Chatbot: {response}")
